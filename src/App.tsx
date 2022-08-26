74266092cc82ddfa35700223192b771b01962604
Luis <lfelipeggarcia@gmail.com>
2022-08-25 23:44:32 -0300

    feat: left-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { MenuLateral } from "./shared/components";
 import { AppThemeProvider } from "./shared/contexts";
 
 function App() {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+        <MenuLateral>
+          <AppRoutes />
+        </MenuLateral>
       </BrowserRouter>
     </AppThemeProvider>
   );
---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,59 @@
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
+interface IMenuLateral {
+  children: React.ReactNode;
+}
+export const MenuLateral: React.FC<IMenuLateral> = ({ children }) => {
+  const theme = useTheme();
+  return (
+    <>
+      <Drawer open={true} variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://docs.google.com/uc?id=1_rfnV80JMYBUIzjS6VmXFoL_oxaoN2H8"
+            />
+          </Box>
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
